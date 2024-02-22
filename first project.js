1bc04cb68f28ad41bab05fb44131d6bf1f19cce9
VladaRey <vladapodzirey@gmail.com>
2024-02-22 08:46:12 +0200

    Added HTML, CSS, JS

--- first project.js
+++ first project.js
@@ -1,1 +1,58 @@
-alert("hello");
+function searchSubmit(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#city");
+  searchCity(cityInput.value);
+}
+
+function searchCity(city) {
+  let apiKey = "9833co795c058e7446f0a2tc1ebbfba5";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function showTemperature(response) {
+  let h1 = document.querySelector("h1");
+  let temperatureElement = response.data.temperature.current;
+  let temperatureValue = document.querySelector("#temperature-value");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#current-time");
+  let date = new Date(response.data.time * 1000);
+  let icon = document.querySelector("#icon");
+
+  icon.innerHTML = `<img src="${response.data.condition.icon_url}" class="temperature-icon" />`;
+  h1.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  temperatureValue.innerHTML = Math.round(temperatureElement);
+}
+
+function formatDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  return `${day} ${hours}:${minutes}`;
+}
+
+let form = document.querySelector("#search-city");
+form.addEventListener("submit", searchSubmit);
+
+searchCity("Norway");
